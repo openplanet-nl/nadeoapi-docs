0f2d61083b482a799c42e43a6a125e5673cbf903
Melissa Geels <melissa@nimble.tools>
2022-07-26 15:19:29 +0200

    Enable caching for 1 second

--- public/components/Controller.php
+++ public/components/Controller.php
@@ -114,9 +114,9 @@ class Controller extends Nin\Controller
 
 	public function getPageIndex()
 	{
-		//TODO: Enable caching
-		//return nf_cache()->take('page_index', 3600, function() {
+		//TODO: Use > 1 second caching
+		return nf_cache()->take('page_index', 1, function() {
 			return $this->getPageIndexInternal();
-		//});
+		});
 	}
 }
